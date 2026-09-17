c2994734070ad23c3b1a3ada96ef8ff811b1a317
richardquadros <richardgabriel022@gmail.com>
2026-09-16 22:06:11 -0300

    Atualizacao

--- src/main/java/br/edu/ifrs/banda/dao/BandaDAOJPA.java
+++ src/main/java/br/edu/ifrs/banda/dao/BandaDAOJPA.java
@@ -14,81 +14,57 @@ public class BandaDAOJPA implements BandaDAO {
     @Override
     public void salvar(Banda banda) {
         EntityManager em = emf.createEntityManager();
-        try {
-            em.getTransaction().begin();
-            em.persist(banda);
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            if (em.getTransaction().isActive()) em.getTransaction().rollback();
-            throw e;
-        } finally {
-            em.close();
-        }
+        em.getTransaction().begin();
+        em.persist(banda);
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public Banda buscarPorId(Long id) {
         EntityManager em = emf.createEntityManager();
-        try {
-            return em.find(Banda.class, id);
-        } finally {
-            em.close();
-        }
+        Banda banda = em.find(Banda.class, id);
+        em.close();
+        return banda;
     }
 
     @Override
     public List<Banda> listarTodos() {
         EntityManager em = emf.createEntityManager();
-        try {
-            return em.createQuery("SELECT b FROM Banda b", Banda.class).getResultList();
-        } finally {
-            em.close();
-        }
+        List<Banda> bandas = em.createQuery("SELECT b FROM Banda b", Banda.class).getResultList();
+        em.close();
+        return bandas;
     }
 
     @Override
     public void atualizar(Banda banda) {
         EntityManager em = emf.createEntityManager();
-        try {
-            em.getTransaction().begin();
-            em.merge(banda);
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            if (em.getTransaction().isActive()) em.getTransaction().rollback();
-            throw e;
-        } finally {
-            em.close();
-        }
+        em.getTransaction().begin();
+        em.merge(banda);
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public void remover(Long id) {
         EntityManager em = emf.createEntityManager();
-        try {
-            em.getTransaction().begin();
-            Banda banda = em.find(Banda.class, id);
-            if (banda != null) {
-                em.remove(banda);
-            }
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            if (em.getTransaction().isActive()) em.getTransaction().rollback();
-            throw e;
-        } finally {
-            em.close();
+        em.getTransaction().begin();
+        Banda banda = em.find(Banda.class, id);
+        if (banda != null) {
+            em.remove(banda);
         }
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public List<Banda> listarPorGenero(String genero) {
         EntityManager em = emf.createEntityManager();
-        try {
-            String jpql = "SELECT b FROM Banda b WHERE b.genero = :genero";
-            TypedQuery<Banda> query = em.createQuery(jpql, Banda.class);
-            query.setParameter("genero", genero);
-            return query.getResultList();
-        } finally {
-            em.close();
-        }
+        String jpql = "SELECT b FROM Banda b WHERE b.genero = :genero";
+        TypedQuery<Banda> query = em.createQuery(jpql, Banda.class);
+        query.setParameter("genero", genero);
+        List<Banda> bandas = query.getResultList();
+        em.close();
+        return bandas;
     }
 }
